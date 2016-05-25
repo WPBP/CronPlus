8055d6ae6a948d1ecd130fc991bd6818c68d0e3f
Daniele Scasciafratte <mte90net@gmail.com>
2016-05-25 19:06:37 +0200

    working on a composer package

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-CronPlus - Plugin/Class for WordPress
-=======
+## CronPlus
+[![License](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)   
 
-Check `cronplus.php` as a example!
+Add and remove Cron job in WordPress easily!
+
+## Example
+
+```
+$args = array(
+    'recurrence' => 'hourly',
+    'schedule' => 'schedule',
+    'name' => 'cronplusexample',
+    'cb' => 'cronplus_example'
+);
+
+function cronplus_example() {
+	echo 123;
+}
+
+$cronplus = new CronPlus( $args );
+$cronplus->schedule_event();
+```
--- cronplus.php
+++ cronplus.php
@@ -1,31 +1,73 @@
 <?php
 
 /**
- * Plugin Name: CronPlus
- * Plugin URI: https://github.com/Mte90/cronplus
- * Description: Facilitates the creation of cron events .
- * Author: Mte90 &  brutalenemy666
- * Author URI: http://mte90.net
- * Version: 1.0.0
- * Text Domain: your-domain
- * Domain Path: /languages
+ * Class CronPlus to facilitate creation of Cron Events
+ * 
+ * @package   CronPlus
+ * @author    Mte90
+ * @license   GPL-2.0+
+ * @link      http://mte90.net
+ * @copyright 2015 GPL
  */
-if ( !defined( 'WPINC' ) ) {
-	die;
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-// Load and initialize class. If you're loading the CronPlus class in another plugin or theme, this is all you need.
-require_once 'class-cronplus.php';
-$args = array(
-    'recurrence' => 'hourly',
-    'schedule' => 'schedule',
-    'name' => 'cronplusexample',
-    'cb' => 'cronplus_example'
-);
+class CronPlus {
 
-function cronplus_example() {
-	echo 123;
+	private $args;
+
+	/**
+	 * Construct the class parameter
+	 * 
+	 * @param array $args
+	 */
+	function __construct( $args ) {
+		$defaults = array(
+		    'recurrence' => 'hourly', //hourly,daily,twicedaily,weekly,monthly
+		    'name' => 'cronplus',
+		    'schedule' => 'schedule' //schedule or single
+		);
+
+		$this->args = wp_parse_args( $args, $defaults );
+		if ( isset( $this->cb ) && isset( $this->name ) ) {
+			add_action( $this->args[ 'name' ], $this->cb );
+		}
+	}
+
+	/**
+	 * Schedule the event
+	 *
+	 * @since    1.0.0
+	 */
+	public function schedule_event() {
+		if ( !wp_next_scheduled( $this->args[ 'name' ] ) ) {
+			if ( $this->args[ 'schedule' ] ) {
+				wp_schedule_event( current_time( 'timestamp' ), $this->args[ 'recurrence' ], $this->args[ 'name' ] );
+			} else {
+				wp_schedule_single_event( $this->args[ 'recurrence' ], $this->args[ 'name' ] );
+			}
+		}
+	}
+
+	/**
+	 * Clear the schedule
+	 *
+	 * @since    1.0.0
+	 */
+	public function clear_schedule() {
+		wp_clear_scheduled_hook( $this->args[ 'name' ] );
+	}
+
+	/**
+	 * UnSchedule the event
+	 *
+	 * @since    1.0.0
+	 */
+	public function unschedule_event() {
+		$timestamp = wp_next_scheduled( $this->args[ 'name' ] );
+		wp_unschedule_event( $timestamp, $this->args[ 'name' ] );
+	}
+
 }
-
-$cronplus = new CronPlus( $args );
-$cronplus->schedule_event();
