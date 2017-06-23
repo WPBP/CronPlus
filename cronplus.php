1ae4d32a40945ad170bccc41cded09068eff4c8c
Daniele Scasciafratte <mte90net@gmail.com>
2017-06-23 15:11:34 +0200

    fix for empty args

--- cronplus.php
+++ cronplus.php
@@ -9,7 +9,7 @@
  */
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) {
-  exit;
+	exit;
 }
 
 /**
@@ -17,70 +17,70 @@ if ( !defined( 'ABSPATH' ) ) {
  */
 class CronPlus {
 
-  /**
-   * Args of the class
-   *
-   * @var     array
-   * @since   1.0.0
-   */
-  private $args;
+	/**
+	 * Args of the class
+	 *
+	 * @var     array
+	 * @since   1.0.0
+	 */
+	private $args;
 
-  /**
-   * Construct the class parameter
-   * 
-   * @param array $args Parameters of class.
-   * @return void
-   */
-  function __construct( $args ) {
-    $defaults = array(
-	  'recurrence' => 'hourly', // Hourly,daily,twicedaily,weekly,monthly
-	  'name' => 'cronplus',
-	  'schedule' => 'schedule', // Schedule or single,
-	  'cb' => '',
-	  'args' => '' // Args passed to the hook
-    );
+	/**
+	 * Construct the class parameter
+	 * 
+	 * @param array $args Parameters of class.
+	 * @return void
+	 */
+	function __construct( $args ) {
+		$defaults = array(
+			'recurrence' => 'hourly', // Hourly,daily,twicedaily,weekly,monthly
+			'name' => 'cronplus',
+			'schedule' => 'schedule', // Schedule or single,
+			'cb' => '',
+			'args' => array( '' ) // Args passed to the hook
+		);
 
-    $this->args = wp_parse_args( $args, $defaults );
-    if ( isset( $this->args['cb'] ) && isset( $this->args['name'] ) ) {
-	add_action( $this->args[ 'name' ], $this->args['cb'] );
-    }
-  }
-
-  /**
-   * Schedule the event
-   *
-   * @since    1.0.0
-   * @return void
-   */
-  public function schedule_event() {
-    if ( !wp_next_scheduled( $this->args[ 'name' ] ) ) {
-	if ( $this->args[ 'schedule' ] === 'schedule' ) {
-	  wp_schedule_event( current_time( 'timestamp' ), $this->args[ 'recurrence' ], $this->args[ 'name' ], $this->args[ 'args' ] );
-	} elseif ( $this->args[ 'schedule' ] === 'single' ) {
-	  wp_schedule_single_event( $this->args[ 'recurrence' ], $this->args[ 'name' ], $this->args[ 'args' ] );
+		$this->args = wp_parse_args( $args, $defaults );
+		if ( isset( $this->args[ 'cb' ] ) && isset( $this->args[ 'name' ] ) ) {
+			add_action( $this->args[ 'name' ], $this->args[ 'cb' ] );
+		}
 	}
-    }
-  }
 
-  /**
-   * Clear the schedule
-   *
-   * @since    1.0.0
-   * @return void
-   */
-  public function clear_schedule() {
-    wp_clear_scheduled_hook( $this->args[ 'name' ] );
-  }
+	/**
+	 * Schedule the event
+	 *
+	 * @since    1.0.0
+	 * @return void
+	 */
+	public function schedule_event() {
+		if ( !wp_next_scheduled( $this->args[ 'name' ] ) ) {
+			if ( $this->args[ 'schedule' ] === 'schedule' ) {
+				wp_schedule_event( current_time( 'timestamp' ), $this->args[ 'recurrence' ], $this->args[ 'name' ], $this->args[ 'args' ] );
+			} elseif ( $this->args[ 'schedule' ] === 'single' ) {
+				wp_schedule_single_event( $this->args[ 'recurrence' ], $this->args[ 'name' ], $this->args[ 'args' ] );
+			}
+		}
+	}
 
-  /**
-   * UnSchedule the event
-   *
-   * @since    1.0.0
-   * @return void
-   */
-  public function unschedule_event() {
-    $timestamp = wp_next_scheduled( $this->args[ 'name' ] );
-    wp_unschedule_event( $timestamp, $this->args[ 'name' ] );
-  }
+	/**
+	 * Clear the schedule
+	 *
+	 * @since    1.0.0
+	 * @return void
+	 */
+	public function clear_schedule() {
+		wp_clear_scheduled_hook( $this->args[ 'name' ] );
+	}
+
+	/**
+	 * UnSchedule the event
+	 *
+	 * @since    1.0.0
+	 * @return void
+	 */
+	public function unschedule_event() {
+		$timestamp = wp_next_scheduled( $this->args[ 'name' ] );
+		wp_unschedule_event( $timestamp, $this->args[ 'name' ] );
+	}
 
 }
